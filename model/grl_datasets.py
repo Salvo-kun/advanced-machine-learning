330bb9a2b8e3218233e869e94b21a86df6f7d3af
AleTola <s303427@studenti.polito.it>
2023-01-11 10:26:08 +0100

    new commit 2

---
+++ model/grl_datasets.py
@@ -0,0 +1,172 @@
+import h5py
+import itertools
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import numpy as np
+from collections import namedtuple
+from PIL import Image
+from sklearn.neighbors import NearestNeighbors
+import faiss
+from glob import glob
+import os
+
+def transform():
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+db_struct = namedtuple("db_struct", ["gallery_images", "gallery_utms", "query_images", "query_utms", "num_gallery", "num_queries",
+                                     "val_pos_dist_threshold", "train_pos_dist_threshold"])
+
+
+def parse_db_struct(dataset_root, gallery_path, query_path):
+    gallery_path = f"{dataset_root}/{gallery_path}"
+    query_path = f"{dataset_root}/{query_path}"
+    if not os.path.exists(gallery_path): raise Exception(f"{gallery_path} does not exist")
+    if not os.path.exists(query_path): raise Exception(f"{query_path} does not exist")
+    db_images = sorted(glob(f"{gallery_path}/**/*.jpg", recursive=True))
+    q_images =  sorted(glob(f"{query_path}/**/*.jpg", recursive=True))
+    db_utms =  np.array([(float(img.split("@")[1]), float(img.split("@")[2])) for img in db_images])
+    q_utms =   np.array([(float(img.split("@")[1]), float(img.split("@")[2])) for img in q_images])
+    num_gallery = len(db_images)
+    num_queries = len(q_images)
+    val_pos_dist_threshold = 25
+    train_pos_dist_threshold = 10
+    return db_struct(db_images, db_utms, q_images, q_utms, num_gallery, num_queries,
+                     val_pos_dist_threshold, train_pos_dist_threshold)
+
+
+class WholeDataset(data.Dataset):
+    # Dataset with both gallery and query images, used for inference (testing and building cache)
+    def __init__(self, dataset_root, gallery_path, query_path):
+        super().__init__()
+        self.transform = transform()
+        self.db_struct = parse_db_struct(dataset_root, gallery_path, query_path)
+        self.images = [dbIm for dbIm in self.db_struct.gallery_images]
+        self.images += [qIm for qIm in self.db_struct.query_images]
+        # Find positives within val_pos_dist_threshold (25 meters), in self.positives_per_query
+        knn = NearestNeighbors(n_jobs=-1)
+        knn.fit(self.db_struct.gallery_utms)
+        self.positives_per_query = knn.radius_neighbors(self.db_struct.query_utms, 
+                                              radius=self.db_struct.val_pos_dist_threshold,
+                                              return_distance=False)
+        self.info = f"< WholeDataset queries: {query_path} ({self.db_struct.num_queries}); gallery: {gallery_path} ({self.db_struct.num_gallery}) >"
+    def __getitem__(self, index):
+        img = Image.open(self.images[index])
+        img = self.transform(img)
+        return img, index
+    def __len__(self):
+        return len(self.images)
+    def __str__(self):
+        return self.info
+    def get_positives(self):
+        return self.positives_per_query
+
+
+def collate_fn(batch):
+    """Creates mini-batch tensors from the list of tuples (query, positive, negatives).
+    Args:
+        data: list of tuple (query, positive, negatives).
+            - query: torch tensor of shape (3, h, w).
+            - positive: torch tensor of shape (3, h, w).
+            - negative: torch tensor of shape (n, 3, h, w).
+    Returns:
+        query: torch tensor of shape (batch_size, 3, h, w).
+        positive: torch tensor of shape (batch_size, 3, h, w).
+        negatives: torch tensor of shape (batch_size, n, 3, h, w).
+    """
+    batch = list(filter(lambda x: x is not None, batch))
+    if len(batch) == 0:
+        return None, None, None, None, None
+    query, positive, negatives, indices = zip(*batch)
+    query = data.dataloader.default_collate(query)
+    positive = data.dataloader.default_collate(positive)
+    neg_counts = data.dataloader.default_collate([x.shape[0] for x in negatives])
+    negatives = torch.cat(negatives, 0)
+    indices = list(itertools.chain(*indices))
+    return query, positive, negatives, neg_counts, indices
+
+
+class QueryDataset(data.Dataset):
+    # Dataset class used for training.
+    def __init__(self, dataset_root, gallery_path, query_path, output_folder):
+        super().__init__()
+        self.output_folder = output_folder
+        self.transform = transform()
+        self.margin = 0.1
+        self.db_struct = parse_db_struct(dataset_root, gallery_path, query_path)
+        self.n_neg_samples = 1000 # Number of negatives to randomly sample
+        self.n_neg = 10 # Number of negatives per query in each batch
+        # Find positives within train_pos_dist_threshold (10 meters), in self.positives_per_query
+        knn = NearestNeighbors(n_jobs=-1)
+        knn.fit(self.db_struct.gallery_utms)
+        self.positives_per_query = list(knn.radius_neighbors(self.db_struct.query_utms,
+                                                              radius=self.db_struct.train_pos_dist_threshold,
+                                                              return_distance=False))
+        self.positives_per_query = [np.sort(positives) for positives in self.positives_per_query]
+        
+        # Make sure that each query in the train set has at least one positive, otherwise it should be deleted
+        queries_without_any_positive = np.where(np.array([len(p) for p in self.positives_per_query]) == 0)[0]
+        if len(queries_without_any_positive) > 0:
+            with open(f"{output_folder}/queries_without_any_positive.txt", "w") as file:
+                for query_index in queries_without_any_positive:
+                    file.write(f"{db_struct.query_images[query_index]}\n")
+            raise Exception(f"There are {len(queries_without_any_positive)} queries in the training " + 
+                            f"set without any positive (within {db_struct.train_pos_dist_threshold} meters) in " +
+                            "the gallery! Please remove these images, as they're not used for training. " +
+                            "The paths of these images have been saved in " + 
+                            f"{output_folder}/queries_without_any_positive.txt")
+        
+        # Find negatives further than val_pos_dist_threshold (25 meters), in self.negatives_per_query
+        positives = knn.radius_neighbors(self.db_struct.query_utms,
+                                         radius=self.db_struct.val_pos_dist_threshold,
+                                         return_distance=False)
+        self.negatives_per_query = []
+        for pos in positives:
+            self.negatives_per_query.append(np.setdiff1d(np.arange(self.db_struct.num_gallery),
+                                                         pos, assume_unique=True))
+        self.neg_cache = [np.empty((0,)) for _ in range(self.db_struct.num_queries)]
+        self.info = f"< QueryDataset queries: {query_path} ({self.db_struct.num_queries}); gallery: {gallery_path} ({self.db_struct.num_gallery}) >"
+    def __getitem__(self, index):
+        with h5py.File(f"{self.output_folder}/cache.hdf5", mode="r") as h5: 
+            cache = h5.get("cache")
+            features_dim = cache.shape[1]
+            queries_offset = self.db_struct.num_gallery
+            query_features = cache[index + queries_offset].astype(np.float32)
+            if np.all(query_features==0):
+                raise Exception(f"For query {self.db_struct.query_images[index]} with index {index} features have not been computed!!!")
+            positives_features = cache[self.positives_per_query[index].tolist()].astype(np.float32)
+            faiss_index = faiss.IndexFlatL2(features_dim)
+            faiss_index.add(positives_features)
+            # Search the best positive (within 10 meters AND nearest in features space)
+            best_pos_dist, best_pos_num = faiss_index.search(query_features.reshape(1, -1), 1)
+            best_pos_dist = best_pos_dist.item()
+            best_pos_index = self.positives_per_query[index][best_pos_num[0]].item()
+            # Sample 1000 negatives randomly and concatenate them with the previous top 10 negatives (neg_cache)
+            neg_samples = np.random.choice(self.negatives_per_query[index], self.n_neg_samples)
+            neg_samples = np.unique(np.concatenate([self.neg_cache[index], neg_samples]))
+            neg_features = np.array([cache[int(neg_sample)] for neg_sample in neg_samples]).astype(np.float32)
+        
+        faiss_index = faiss.IndexFlatL2(features_dim)
+        faiss_index.add(neg_features)
+        # Search the 10 nearest negatives (further than 25 meters and nearest in features space)
+        neg_dist, neg_nums = faiss_index.search(query_features.reshape(1, -1), self.n_neg)
+        neg_dist = neg_dist.reshape(-1)
+        neg_nums = neg_nums.reshape(-1)
+        violating_neg = (neg_dist ** 0.5) < (best_pos_dist ** 0.5) + (self.margin ** 0.5)
+        if np.sum(violating_neg) < 1:
+            return None # If none are violating then skip this query
+        neg_nums = neg_nums[violating_neg][:self.n_neg]
+        neg_indices = neg_samples[neg_nums].astype(np.int32)
+        self.neg_cache[index] = neg_indices # Update nearest negatives in neg_cache
+        query = self.transform(Image.open(self.db_struct.query_images[index]))
+        positive = self.transform(Image.open(self.db_struct.gallery_images[best_pos_index]))
+        negatives = [self.transform(Image.open(self.db_struct.gallery_images[i])) for i in neg_indices]
+        negatives = torch.stack(negatives, 0)
+        return query, positive, negatives, [index, best_pos_index] + neg_indices.tolist()
+    def __len__(self):
+        return self.db_struct.num_queries
+    def __str__(self):
+        return self.info
---
+++ model/vlad_network.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+
+class AttenNetVLAD(nn.Module):
+    def __init__(self, backbone, netvlad_layer, grl_discriminator=None, attention=False):
+        super(AttenNetVLAD, self).__init__()
+        self.backbone = backbone
+        self.netvlad_layer = netvlad_layer
+        self.grl_discriminator = grl_discriminator
+        self.weight_softmax = self.backbone.fc.weight
+        self.attention = attention
+    
+    def forward(self, input, grl=False, mode="vlad"):
+        if grl:
+            _, out, _ = self.backbone(input)
+            out = self.grl_discriminator(out)
+        else:
+            if self.attention:
+                fc_out, feature_conv, feature_convNBN = self.backbone(input)
+                bz, nc, h, w = feature_conv.size()
+                feature_conv_view = feature_conv.view(bz, nc, h * w)
+                probs, idxs = fc_out.sort(1, True)
+                class_idx = idxs[:, 0]
+                scores = self.weight_softmax[class_idx].to(input.device)
+                cam = torch.bmm(scores.unsqueeze(1), feature_conv_view)
+                attention_map = F.softmax(cam.squeeze(1), dim=1)
+                attention_map = attention_map.view(attention_map.size(0), 1, h, w)
+                attention_features = feature_convNBN * attention_map.expand_as(feature_conv)
+                
+                if mode == "feat":
+                    out = attention_features
+                elif mode == "vlad":
+                    out = self.netvlad_layer(attention_features)
+            else:
+                _, out, _ = self.backbone(input)
+                if mode == "vlad":
+                    out = self.netvlad_layer(out)
+        return out
+
+
+# based on https://github.com/lyakaap/NetVLAD-pytorch/blob/master/netvlad.py
+class NetVLAD(nn.Module):
+    """NetVLAD layer implementation"""
+    
+    def __init__(self, num_clusters=64, dim=128, normalize_input=True):
+        """
+        Args:
+            num_clusters : int
+                The number of clusters
+            dim : int
+                Dimension of descriptors
+            alpha : float
+                Parameter of initialization. Larger value is harder assignment.
+            normalize_input : bool
+                If true, descriptor-wise L2 normalization is applied to input.
+        """
+        super(NetVLAD, self).__init__()
+        
+        # Vlad module
+        self.num_clusters = num_clusters
+        self.dim = dim
+        self.alpha = 0
+        self.normalize_input = normalize_input
+        self.conv = nn.Conv2d(dim, num_clusters, kernel_size=(1, 1), bias=False)
+        self.centroids = nn.Parameter(torch.rand(num_clusters, dim))
+    
+    def init_params(self, clsts, traindescs):
+        # Init vlad params
+        clsts_assign = clsts / np.linalg.norm(clsts, axis=1, keepdims=True)
+        dots = np.dot(clsts_assign, traindescs.T)
+        dots.sort(0)
+        dots = dots[::-1, :] # sort, descending
+        
+        self.alpha = (-np.log(0.01) / np.mean(dots[0,:] - dots[1,:])).item()
+        self.centroids = nn.Parameter(torch.from_numpy(clsts))
+        self.conv.weight = nn.Parameter(torch.from_numpy(self.alpha*clsts_assign).unsqueeze(2).unsqueeze(3))
+        self.conv.bias = None
+    
+    def __vlad_compute_original__(self, x_flatten,soft_assign, N, D):
+        vlad = torch.zeros([N, self.num_clusters, D], dtype=x_flatten.dtype, device=x_flatten.device) #24 64 256
+        for D in range(self.num_clusters): # slower than non-looped, but lower memory usage
+            residual = x_flatten.unsqueeze(0).permute(1, 0, 2, 3) - \
+                    self.centroids[D:D+1, :].expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
+            residual = residual * soft_assign[:,D:D+1,:].unsqueeze(2)   # 24 1 256 961 * 24 1 1 961  = 24 1 256 961
+            vlad[:,D:D+1,:] = residual.sum(dim=-1)     #vlas.size = 24 64 256 961
+        vlad = F.normalize(vlad, p=2, dim=2)  # intra-normalization
+        vlad = vlad.view(N, -1)  # flatten
+        vlad = F.normalize(vlad, p=2, dim=1)  # L2 normalize
+        return vlad
+    
+    def forward(self, x):
+        N, D, H, W = x.shape[:]
+        if self.normalize_input:
+            x = F.normalize(x, p=2, dim=1)  # across descriptor dim
+        x_flatten = x.view(N, D, -1)
+        soft_assign = self.conv(x).view(N, self.num_clusters, -1)
+        soft_assign = F.softmax(soft_assign, dim=1)
+        vlad = self.__vlad_compute_original__(x_flatten, soft_assign, N, D)
+        return vlad
